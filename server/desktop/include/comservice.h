ec54a99d8770383abc7c3a1eea5220fa00779180
Oliver <o.joisten@live.se>
2023-09-08 21:30:29 +0200

    chore(.gitignore): add .vscode and build directories to gitignore chore(CMakeLists.txt): remove trailing whitespace in CLIENT_HEADERS variable docs(comservice.h): add file-level documentation for the COMService class refactor(comservice.h): remove redundant comments and reformat code for readability refactor(setting.h): reorganize namespaces and remove redundant comments

--- server/desktop/include/comservice.h
+++ server/desktop/include/comservice.h
@@ -1,3 +1,11 @@
+/**
+ * @file comservice.h
+ * @brief Header file for the COMService class.
+ *
+ * This file contains the declaration of the COMService class, which is responsible for handling communication with external devices.
+ * The class provides methods for setting various parameters such as speed, temperature, battery level, and lights.
+ * It also contains a protected buffer for storing signals and a mutex for thread safety.
+ */
 #ifndef COMSERVICE_H
 #define COMSERVICE_H
 
@@ -6,81 +14,26 @@
 #include <atomic>
 #include "setting.h"
 
-/**
- * @brief The COMService class provides a base class for communication services.
- *
- * This class provides a base implementation for communication services. It includes
- * methods for setting various parameters such as speed, temperature, and battery level,
- * as well as methods for controlling the left and right lights. The class also includes
- * a protected buffer for storing signal data, as well as a mutex for thread safety.
- *
- * To use this class, you should subclass it and implement the run() method. This method
- * should contain the main logic for your communication service.
- */
 class COMService
 {
     void insert(uint32_t start, uint32_t length, uint32_t value);
 
 protected:
-    std::mutex mtx;                  /**< Mutex for thread safety */
-    std::atomic<bool> status{false}; /**< Atomic boolean for status */
+    std::mutex mtx;
+    std::atomic<bool> status{false};
+    uint8_t Buffer[Setting::Signal::BUFSIZE]{0};
 
-    uint8_t Buffer[Setting::Signal::BUFSIZE]{0}; /**< Buffer for storing signal data */
-
-    /**
-     * @brief The run() method should contain the main logic for your communication service.
-     *
-     * This method should contain the main logic for your communication service. It will be
-     * called by the start() method, which should be implemented in your subclass.
-     */
     virtual void run(void) = 0;
 
 public:
-    /**
-     * @brief Get the status of the communication service.
-     *
-     * @return true if the communication service is running, false otherwise.
-     */
     bool getStatus(void) { return status; }
-
-    /**
-     * @brief Set the speed parameter.
-     *
-     * @param value The new value for the speed parameter.
-     */
     void setSpeed(uint32_t value);
-
-    /**
-     * @brief Set the temperature parameter.
-     *
-     * @param value The new value for the temperature parameter.
-     */
     void setTemperature(uint32_t value);
-
-    /**
-     * @brief Set the battery level parameter.
-     *
-     * @param value The new value for the battery level parameter.
-     */
     void setBatteryLevel(uint32_t value);
-
-    /**
-     * @brief Set the left light.
-     *
-     * @param value true to turn on the left light, false to turn it off.
-     */
     void SetLightLeft(bool value);
-
-    /**
-     * @brief Set the right light.
-     *
-     * @param value true to turn on the right light, false to turn it off.
-     */
     void SetLightRight(bool value);
 
     virtual ~COMService() = default;
-
-private:
 };
 
-#endif
+#endif // COMSERVICE_H
--- shared/setting.h
+++ shared/setting.h
@@ -36,63 +36,62 @@ namespace Setting
             constexpr int MAX{240};  /**<The maximum speed value*/
             constexpr int START{0};  /**<The start bit of the speed signal*/
             constexpr int LENGTH{8}; /**<The length of the speed signal*/
-
-            namespace Temperature
-            {
-                constexpr int MIN{-60};  /**<The minimum temperature value*/
-                constexpr int MAX{60};   /**<The maximum temperature value*/
-                constexpr int START{8};  /**<The start bit of the temperature signal*/
-                constexpr int LENGTH{7}; /**<The length of the temperature signal*/
-            }
-            namespace BatteryLevel
-            {
-                constexpr int MIN{0};    /**<The minimum battery level value*/
-                constexpr int MAX{100};  /**<The maximum battery level value*/
-                constexpr int START{15}; /**<The start bit of the battery level signal*/
-                constexpr int LENGTH{7}; /**<The length of the battery level signal*/
-            }
-            namespace Light
-            {
-                namespace Left
-                {
-                    constexpr int MIN{0};    /**<The minimum light value*/
-                    constexpr int MAX{1};    /**<The maximum light value*/
-                    constexpr int START{22}; /**<The start bit of the light signal*/
-                    constexpr int LENGTH{1}; /**<The length of the light signal*/
-                }
-                namespace Right
-                {
-                    constexpr int MIN{0};    /**<The minimum light value*/
-                    constexpr int MAX{1};    /**<The maximum light value*/
-                    constexpr int START{23}; /**<The start bit of the light signal*/
-                    constexpr int LENGTH{1}; /**<The length of the light signal*/
-                }
-
-            }
-
-            constexpr int BUFSIZE{3};  /**<The size of the buffer*/
-            constexpr int BYTE_LEN{8}; /**<The length of a byte*/
         }
+        namespace Temperature
+        {
+            constexpr int MIN{-60};  /**<The minimum temperature value*/
+            constexpr int MAX{60};   /**<The maximum temperature value*/
+            constexpr int START{8};  /**<The start bit of the temperature signal*/
+            constexpr int LENGTH{7}; /**<The length of the temperature signal*/
+        }
+        namespace BatteryLevel
+        {
+            constexpr int MIN{0};    /**<The minimum battery level value*/
+            constexpr int MAX{100};  /**<The maximum battery level value*/
+            constexpr int START{15}; /**<The start bit of the battery level signal*/
+            constexpr int LENGTH{7}; /**<The length of the battery level signal*/
+        }
+        namespace Light
+        {
+            namespace Left
+            {
+                constexpr int MIN{0};    /**<The minimum light value*/
+                constexpr int MAX{1};    /**<The maximum light value*/
+                constexpr int START{22}; /**<The start bit of the light signal*/
+                constexpr int LENGTH{1}; /**<The length of the light signal*/
+            }
+            namespace Right
+            {
+                constexpr int MIN{0};    /**<The minimum light value*/
+                constexpr int MAX{1};    /**<The maximum light value*/
+                constexpr int START{23}; /**<The start bit of the light signal*/
+                constexpr int LENGTH{1}; /**<The length of the light signal*/
+            }
+
+        }
+
+        constexpr int BUFSIZE{3};  /**<The size of the buffer*/
+        constexpr int BYTE_LEN{8}; /**<The length of a byte*/
+    }
 
 #ifdef UARTCOM
-        namespace UART_Connection
-        {
-            constexpr int BAUDRATE{115200};         /**<The baudrate of the UART connection*/
-            constexpr char PORT[] = "/dev/ttyUSB0"; /**<The port of the UART connection*/
-        }
-#else
-        namespace tcp_connection
-        {
-            namespace tcp_port
-            {
-                constexpr int PORT{15045}; /**<The port of the TCP connection*/
-            }
-            namespace tcp_ip
-            {
-                constexpr char IP[] = "127.0.0.1"; /**<The IP of the TCP connection*/
-            }
-        }
-#endif // UARTCOM
+    namespace UART_Connection
+    {
+        constexpr int BAUDRATE{115200};         /**<The baudrate of the UART connection*/
+        constexpr char PORT[] = "/dev/ttyUSB0"; /**<The port of the UART connection*/
     }
-#endif // SETTING_H
+#else
+    namespace tcp_connection
+    {
+        namespace tcp_port
+        {
+            constexpr int PORT{15045}; /**<The port of the TCP connection*/
+        }
+        namespace tcp_ip
+        {
+            constexpr char IP[] = "127.0.0.1"; /**<The IP of the TCP connection*/
+        }
+    }
+#endif // UARTCOM
 }
+#endif // SETTING_H
